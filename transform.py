50b4c7fcb3235b04cfd5829952d0413e1e8b01f7
Grzegorz Kazana <kazana.grzegorz@gmail.com>
2019-10-26 09:40:13 +0200

    added notebook for task1-evaluation

--- transform.py
+++ transform.py
@@ -4,10 +4,11 @@ import pathlib
 import cv2
 import numpy as np
 
-DEFAULT_SIZE_LENGTH = 244
+DEFAULT_SIZE_LENGTH = 224
 
 # def mirror_fill(image: np.ndarray, desired_shape: typing.Tuple[int, int]) -> np.ndarray:
 #     ...
+
 
 def mirror_fill(image: np.ndarray, side_length: int) -> np.ndarray:
     height, width, channels_count = image.shape
@@ -21,8 +22,10 @@ def mirror_fill(image: np.ndarray, side_length: int) -> np.ndarray:
         transformed_image[height:, :, :] = image[height : 2 * height - max_dim - 1 : -1, :, :]
     return cv2.resize(transformed_image * 1 / 256, (side_length, side_length))
 
+
 def stretch(image: np.ndarray, desired_shape: typing.Tuple[int, int]) -> np.ndarray:
     # print(image.shape, desired_shape)
+
 
 def stretch(image: np.ndarray, side_length: int) -> np.ndarray:
     return cv2.resize(image, (side_length, side_length))
